ca273eb39f667f87fd3f6a4a15c533bef0da5add
akash112saini <122890830+akash112saini@users.noreply.github.com>
2024-12-18 11:33:12 +0530

    Update main.js

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,191 +1,79 @@
+// JS for tab button 
+const tabContainer = document.getElementById('tab-btn47');
+let currentPanel = '1';
 
-document.addEventListener('DOMContentLoaded', () => {
-    const carousel = document.querySelector('#bannerCarousel');
-    const carouselInstance = bootstrap.Carousel.getOrCreateInstance(carousel);
+if (tabContainer) {
+    const tabs = tabContainer.querySelectorAll('.tab-btn');
+    const panels = document.querySelectorAll('.panel');
 
-    // Pause carousel on hover
-    carousel.addEventListener('mouseenter', () => carouselInstance.pause());
-    carousel.addEventListener('mouseleave', () => carouselInstance.cycle());
-  });
+    function switchTabs(e) {
+        const targetTab = e.target;
+        const tabId = targetTab.getAttribute('data-id');
 
-
-    // Navbar Animation
-    var tabsNewAnim = $('#navbar-animmenu');
-    var activeItemNewAnim = tabsNewAnim.find('.active');
-    var activeWidthNewAnimWidth = activeItemNewAnim.innerWidth();
-    var itemPosNewAnimLeft = activeItemNewAnim.position();
-
-    $(".hori-selector").css({
-        "left": itemPosNewAnimLeft.left + "px",
-        "width": activeWidthNewAnimWidth + "px"
-    });
-
-    $("#navbar-animmenu").on("click", "li", function () {
-        $('#navbar-animmenu ul li').removeClass("active");
-        $(this).addClass('active');
-        var activeWidthNewAnimWidth = $(this).innerWidth();
-        var itemPosNewAnimLeft = $(this).position();
-        $(".hori-selector").css({
-            "left": itemPosNewAnimLeft.left + "px",
-            "width": activeWidthNewAnimWidth + "px"
+        // Hide all panels and remove 'active' class from all tabs
+        tabs.forEach(tab => {
+            tab.classList.remove('active');
         });
+        panels.forEach(panel => {
+            panel.classList.remove('active');
+        });
+
+        // Show the corresponding panel and add 'active' class to the clicked tab
+        document.getElementById(`tab-panel${tabId}`).classList.add('active');
+        targetTab.classList.add('active');
+    }
+
+    // Set the initial active tab and panel
+    tabs[0].classList.add('active');
+    panels[0].classList.add('active');
+
+    // Add event listeners to all tabs
+    tabs.forEach(tab => {
+        tab.addEventListener('click', switchTabs);
     });
+}
 
-    $(document).on("click", "#navbar-animmenu ul li", function () {
-      var tabId = $(this).data("tab");
-      $("#navbar-animmenu ul li").removeClass("active");
-      $(this).addClass("active");
-      $(".tab-content").removeClass("active");
-      $("#" + tabId).addClass("active");
-      });
 
-      document.addEventListener('DOMContentLoaded', function() {
-        var activePanel = document.querySelector('.panel.active');
-        if (activePanel) {
-            var activeAccordion = activePanel.previousElementSibling;
-            activePanel.style.display = "block";
-            activeAccordion.querySelector(".icon").innerHTML = "-";
-            activeAccordion.classList.add("clicked");
+// creating the scrolling marquee feature in the recruiters section
+
+function createRecruiterDOM(itemCount) {
+    let firstHtml = '';
+    let secondHtml = '';
+    for (let i = 1; i <= itemCount; i++) {
+        
+        if(i <= itemCount/2)
+        {
+            firstHtml += `<div class="scrolling-div"><img src="./asset/img/recruiters/image_${i}.webp" class="recruiters-img" alt=""></div>`;
         }
-    });
-    
-     // 
-     
-    
-        const reviews = [{
-            name: "Speaks about College of Computing Sciences & IT",
-            text: "TMU, as a University is the cornerstone of my success as a Technical Architect. The transformative education equipped me with vital skills and mentorship from faculty. Practical learning and industry exposure provided confidence to excel in my profession. Grateful for TMU's profound impact on my career journey.",
-            img: "asset/img/testimonial/1.png",
-            profile: {
-                img: "asset/img/testimonial/1.png",
-                name: "Rajeev Mishra",
-                link: "https://www.linkedin.com/in/rajeevmishra20/",
-                designation: "Technology Architect",
-                company: "Bearing Point, Germany",
-                course: "B.Tech CSE",
-                graduationYear: "2014",
-            },
-        },
+
+        else
         {
-            name: "Speaks about TMIMT College of Management",
-            text: "Teerthanker Mahaveer University (TMU) played a vital role in my success as Head of Saudi German Hospitals Group in Dubai. The transformative MBA experience provided essential skills and support from faculty mentors. Practical learning and industry exposure instilled confidence for leadership, and I am forever grateful for TMU's impact on my career.",
-            img: "asset/img/testimonial/2.png",
-            profile: {
-                img: "asset/img/testimonial/2.png",
-                name: "Shobhit Jain",
-                link: "https://www.linkedin.com/in/shobhit-jain-49197ba0",
-                designation: "Group Director - Talent Acquisition",
-                company: "Saudi German Hospitals Group",
-                 course:"MBA",
-                graduationYear: "2011",
-            },
-        },
-        {
-            name: "Speaks about Medical College & Research Centre",
-            text: "TMU, Medical College & Research Centre has been the bedrock of my success as an MBBS doctor. The comprehensive medical education and mentorship from faculty have honed my skills and knowledge. With practical training and industry exposure, TMU empowered me to make a meaningful impact in healthcare. Forever grateful to TMU.",
-            img: "asset/img/testimonial/3.png",
-            profile: {
-                img: "asset/img/testimonial/3.png",
-                name: "Radhika Mathur",
-                link: "#",
-                designation: "Resident Physician",
-                company: "HCA Florida Oak Hill Hospital",
-                course: "MBBS",
-                graduationYear: "2019",
-            },
-        },
-        {
-            name: "Speaks about Engineering College",
-            text: "I am deeply grateful to TMU for laying the foundation of my academic and professional journey. Graduating with a Master of Computer Applications in 2005 as a First Division Honors topper, I can confidently say that the education and mentorship I received at TMU were pivotal in shaping my career. The institute’s strong curriculum and emphasis on practical learning equipped me with the skills necessary to excel in the technology field. Today, as a Senior Software Architect at HCL Software in Noida, I often reflect on the solid foundation and critical thinking skills I developed at TMU. The institute will always hold a special place in my heart, and I am proud to be an alumnus of such a distinguished institution. ",
-            img: "asset/img/testimonial/1.png",
-            profile: {
-                img: "asset/img/testimonial/1.png",
-                name: "Ashish Bhatnagar",
-                link: "https://www.linkedin.com/in/abhatnagar83",
-                designation: "Senior Software Architect",
-                company: "HCL Software, Noida",
-                course: "MCA",
-                graduationYear: "2005",
-            },
-        },
-        {
-            name: "Speaks about College of Computing Sciences & IT",
-            text: "TMU, as a University is the cornerstone of my success as a Technical Architect. The transformative education equipped me with vital skills and mentorship from faculty. Practical learning and industry exposure provided confidence to excel in my profession. Grateful for TMU's profound impact on my career journey.",
-            img: "asset/img/testimonial/2.png",
-            profile: {
-                img: "asset/img/testimonial/2.png",
-                name: "Rajeev Mishra",
-                link: "https://www.linkedin.com/in/rajeevmishra20/",
-                designation: "Technology Architect",
-                company: "Bearing Point, Germany",
-                course: "B.Tech CSE",
-                graduationYear: "2014",
-            },
-        },
-        {
-            name: "Speaks about TMIMT College of Management",
-            text: "Teerthanker Mahaveer University (TMU) played a vital role in my success as Head of Saudi German Hospitals Group in Dubai. The transformative MBA experience provided essential skills and support from faculty mentors. Practical learning and industry exposure instilled confidence for leadership, and I am forever grateful for TMU's impact on my career.",
-            img: "asset/img/testimonial/3.png",
-            profile: {
-                img: "asset/img/testimonial/3.png",
-                name: "Shobhit Jain",
-                link: "https://www.linkedin.com/in/shobhit-jain-49197ba0",
-                designation: "Group Director - Talent Acquisition",
-                company: "Saudi German Hospitals Group",
-                 course:"MBA",
-                graduationYear: "2011",
-            },
-        },
-        {
-            name: "Speaks about Medical College & Research Centre",
-            text: "TMU, Medical College & Research Centre has been the bedrock of my success as an MBBS doctor. The comprehensive medical education and mentorship from faculty have honed my skills and knowledge. With practical training and industry exposure, TMU empowered me to make a meaningful impact in healthcare. Forever grateful to TMU.",
-            img: "asset/img/testimonial/2.png",
-            profile: {
-                img: "asset/img/testimonial/2.png",
-                name: "Radhika Mathur",
-                link: "#",
-                designation: "Resident Physician",
-                company: "HCA Florida Oak Hill Hospital",
-                course: "MBBS",
-                graduationYear: "2019",
-            },
-        },
-    ];
-    
-    let currentReviewIndex = 0;
-    
-    function showReview(index) {
-        const review = reviews[index];
-        document.querySelector('.testimonial-title').innerText = review.name;
-        document.querySelector('.review-text').innerText = review.text;
-        document.getElementById('profile-picture').src = review.profile.img;
-        document.getElementById('profile-name').innerText = review.profile.name;
-        document.getElementById('profile-link').href = review.profile.link;
-        document.getElementById('profile-designation').innerText = review.profile.designation;
-        document.getElementById('profile-company').innerText = review.profile.company;
-        document.getElementById('profile-course').innerText = review.profile.course;
-        document.getElementById('profile-graduationYear').innerText = review.profile.graduationYear;
-    
-        document.querySelectorAll('.thumb').forEach((thumb, i) => {
-            thumb.classList.toggle('active-thumb', i === index);
-        });
-    }
-    
-    function showNextReview() {
-        currentReviewIndex = (currentReviewIndex + 1) % reviews.length;
-        showReview(currentReviewIndex);
-    }
-    
-    function showPreviousReview() {
-        currentReviewIndex = (currentReviewIndex - 1 + reviews.length) % reviews.length;
-        showReview(currentReviewIndex);
-    }
-    
-    function displayReviewByIndex(index) {
-        currentReviewIndex = index;
-        showReview(currentReviewIndex);
+            secondHtml += `<div class="scrolling-div"><img src="./asset/img/recruiters/image_${i}.webp" class="recruiters-img" alt=""></div>`;
+        }
     }
 
+    return {
+        firstHtml : firstHtml,
+        secondHtml : secondHtml
+    }
+}
+
+function addRecruiters() {
+
+    const itemCount = 24;
+    const scrollingDiv = document.querySelector('.scrolling-divs');
+    const scrollingDiv2 = document.querySelector('.scrolling-divs.slower');
+
+    const {firstHtml, secondHtml} = createRecruiterDOM(itemCount);
+
+    if(scrollingDiv )
+    {
+        scrollingDiv.innerHTML = firstHtml + firstHtml + firstHtml; 
+    }
+    if(scrollingDiv2)
+    {
+        scrollingDiv2.innerHTML = secondHtml + secondHtml + secondHtml;
+    }
 }
 
 addRecruiters();
@@ -368,7 +256,6 @@ addRecruiters();
         currentReviewIndex = index;
         showReview(currentReviewIndex);
     }
-
     
     showReview(currentReviewIndex);
     
@@ -399,7 +286,6 @@ addRecruiters();
          $(".custom-tab-content div").removeClass("active-content");
          $("#" + tabId).addClass("active-content");
      });
-
 
      document.addEventListener('DOMContentLoaded', () => {
         const galleryImages = document.querySelectorAll('.gallery-img');
